dc55fbf0427fb6f52dfd8af9a2e51d2eb7eb1900
Michael <friendstak@gmail.com>
2012-08-18 18:10:38 +0200

    Zacząłem kodzić to w konsoli.
    Wstępne projektowanie:
    Projektowanie
    Menu będzie opierać się na switchu.  Będzie to wybór.
    Zakupy, administracja, aktualne promocje.
    Jeżeli wejdzie się w zakupy. Można kupić zegarek. Kosztują po 50zł.
    Jeżeli się 10 kupi. Płaci się 450zł ( 10 % ). Jeżeli kupisz 20 sztuk
    budzików płacisz 800zł.
    
    Hasło musi być hermetyczne.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KoszykKonsola
+{
+    class Program
+    {
+        public string haslo { get; private set; }
+        public string SprawdzHaslo(string Haslo)
+        {
+            Haslo="123456";
+            haslo = Haslo;
+            return haslo;
+        }
+
+        static void Main(string[] args)
+        {
+            #region Zmienne
+            int menu=0;
+            string phaslo;
+            #endregion
+           
+            #region Acesory
+            
+            #endregion
+
+            Console.WriteLine("Wybierz pozycje z menu: ");
+            Console.WriteLine(" 1 ) Panel administracyjny ");
+            Console.WriteLine(" 2 ) Sklep ");
+            Console.WriteLine(" 3 ) Aktualne promocje ");
+            Console.WriteLine(" 4 ) Wyjście ");
+
+       
+            menu = int.Parse(Console.ReadLine());
+          
+            switch (menu)
+            {
+                case 1:
+                      Console.WriteLine("Witaj sprawdzimy czy jesteś administratorem. Podaj hasło: ");
+                      phaslo = Console.ReadLine();
+                      if (true)
+                      {
+                          //Console.WriteLine("Witaj Administratorze. ");
+                          //Console.WriteLine(" 1) Dodaj produkt ");
+                          //Console.WriteLine(" 2)
+
+                      }
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    Console.WriteLine(" Koniec ");
+                   // Console.Exit;
+                    break;
+                default:
+                    Console.WriteLine(" Nie ma takiej pozycji  w menu ");
+                    break;
+            }
+        }
+    }
+}
+
+
+    
+
