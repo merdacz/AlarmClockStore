50d4afb041e0d1a7dd2a1cf166a8072ec3e78a5b
Michał <friendstak@gmail.com>
2012-11-11 13:12:44 +0100

    new

---
+++ Cart.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClockAlarmStore
+{
+    class Cart
+    {
+        public List<ShopItem> Items = new List<ShopItem>();
+
+        public void AddItem(int id)
+        {
+            ShopItem newItem = new ShopItem(id);
+            if(Items.Contains(newItem))
+            {
+                foreach (var shopItem in Items)
+                {
+                    if(shopItem.Equals(newItem))
+                    {
+                        shopItem.AmountItem++;
+                    }
+                    else
+                    {
+                        newItem.AmountItem = 1;
+                        Items.Add(newItem);
+                    }
+                }
+            }
+     
+           
+        }
+        public void RemoveItem(int id)
+        {
+            ShopItem shopItemRemove = new ShopItem(id);
+            Items.Remove(shopItemRemove);
+
+        }
+    }
+}
---
+++ ShopItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClockAlarmStore
+{
+    class ShopItem
+    {
+        /// <summary>
+        /// Oddzielenie "logika" przedmiotu, od sklepu
+        /// </summary>
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int Price
+        {
+            get { return id; }
+            set { price = value; }
+        }
+          
+        public int AmountItem
+        {
+            get { return amountItem; }
+            set { amountItem = value; }
+        }
+
+        private int id;
+        private int price;
+        private int amountItem;
+
+        public ShopItem(int id)
+        {
+
+        }
+
+    }
+}
