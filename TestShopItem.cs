52a1ef1887f06c60acf7b61ad7709fbc3f4a4c17
Michał <friendstak@gmail.com>
2012-11-11 17:06:01 +0100

    test logic ShopItem

---
+++ TestShopItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace ClockAlarmStore
+{
+   [TestFixture]
+    public class TestCart
+   {
+      [Test]
+       public void CartConstructor()
+      {
+          Cart cart=new Cart();
+          Assert.IsNotNull(cart);
+      }
+
+       [Test] 
+      public void ShopItemMethodTest()
+       {
+           int id = 0;
+          ShopItem item=new ShopItem(id);
+          
+           //int result= cart.AddItem(id);
+
+           Assert.AreEqual(0,item.Id);
+       }
+
+
+   }
+  
+
+}
